283528e43b933725b58ad9d3e4c9b7728acc9626
Chandler Morrison <cwm795@gmail.com>
2017-10-05 18:29:41 -0500

    added some variables and added base code to start off with

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log("hi");
+let input = document.getElementById("input");
+let c = document.getElementById("c");
+let f = document.getElementById("f");
+let output = document.getElementById("output");
+let button = document.getElementById("button");
+
+
+
+
+function toCelsius () {
+
+}
+
+function toFahrenheit () {
+
+}
+
+// Get a reference to the button element in the DOM
+var button = document.getElementById("converter");
+
+// This function should determine which conversion should
+// happen based on which radio button is selected.
+function determineConverter (clickEvent) {
+  console.log("event", clickEvent);
+}
+
+// Assign a function to be executed when the button is clicked
+button.addEventListener("click", determineConverter)
